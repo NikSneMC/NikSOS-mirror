6c5b93e9a41e89e1538af172f584c46fc78ad446
NikSne <commits@niksne.ru>
2024-09-02 07:34:27 +0300

    refactor (programs/editors/jetbrains): remove boilerplate && unused IDEs

--- home/niksne/programs/editors/jetbrains.nix
+++ home/niksne/programs/editors/jetbrains.nix
@@ -1,12 +1,12 @@
-{npkgs, ...}: let
+{npkgs, ...}:let
   ides = with npkgs.jetbrains; [
     idea-ultimate
-    pycharm-professional
-    webstorm
-    rust-rover
-    goland
-    phpstorm
-    clion
+    # pycharm-professional
+    # webstorm
+    # rust-rover
+    # goland
+    # phpstorm
+    # clion
   ];
 in {
   programs.jetbrains-remote = {
