018eca27e1b40075bcf4a2e3152ed21b769dc4e4
NikSne <commits@niksne.ru>
2024-11-21 23:37:10 +0300

    refactor: use pipe operator

--- home/niksne/programs/editors/jetbrains.nix
+++ home/niksne/programs/editors/jetbrains.nix
@@ -1,13 +1,13 @@
 {npkgs, ...}: let
   ides = with npkgs.jetbrains; [
-    idea-ultimate
-    # pycharm-professional
-    # webstorm
-    # rust-rover
-    # goland
-    # phpstorm
-    # clion
-  ];
+      idea-ultimate
+      # pycharm-professional
+      # webstorm
+      # rust-rover
+      # goland
+      # phpstorm
+      # clion
+    ];
 in {
   programs.jetbrains-remote = {
     enable = true;
