bba4bcce9155d827bb2c36bd803152f1c24c5211
NikSne <commits@niksne.ru>
2026-05-10 22:36:54 +0300

    feat(kanata): add chords for faster shifts

--- system/services/kanata/default.nix
+++ system/services/kanata/default.nix
@@ -4,6 +4,7 @@
 
     keyboards.default = {
       extraDefCfg = ''
+        concurrent-tap-hold yes
         process-unmapped-keys yes
       '';
       config = builtins.readFile (./. + "/main.kbd");
