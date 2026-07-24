ab2c086ad270d58802412bc3331f6d12fbc527df
NikSne <commits@niksne.ru>
2026-07-24 21:23:44 +0300

    fix(renga,tobichi): don't use noctalia for ani-cli

--- home/terminal/programs/ani-cli.nix
+++ home/terminal/programs/ani-cli.nix
@@ -1,20 +1,30 @@
 {
   config,
+  lib,
+  lib',
+  osConfig,
   pkgs,
   ...
-}: {
-  home.packages = [
-    (pkgs.ani-cli.overrideAttrs (old: {
-      postPatch =
-        (old.postPatch or "")
-        + ''
-          substituteInPlace ani-cli \
-            --replace-fail \
-              '[ "$use_external_menu" = "2" ] && dmenu -l 20 -p "$2"' \
-              '[ "$use_external_menu" = "2" ] && noctalia dmenu -p "$2"'
-        '';
+}: let
+  inherit (lib) optionalAttrs;
 
-      buildInputs = (old.buildInputs or []) ++ [config.programs.noctalia.package];
-    }))
+  notForHosts = lib'.notForHosts osConfig;
+in {
+  home.packages = [
+    (
+      pkgs.ani-cli.overrideAttrs (old:
+        notForHosts ["tobichi" "renga"] optionalAttrs {
+          postPatch =
+            (old.postPatch or "")
+            + ''
+              substituteInPlace ani-cli \
+                --replace-fail \
+                  '[ "$use_external_menu" = "2" ] && dmenu -l 20 -p "$2"' \
+                  '[ "$use_external_menu" = "2" ] && noctalia dmenu -p "$2"'
+            '';
+
+          buildInputs = (old.buildInputs or []) ++ [config.programs.noctalia.package];
+        })
+    )
   ];
 }
