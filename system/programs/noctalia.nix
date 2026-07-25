dae3c0bdd6eb83acc9bd687710894d9961916587
NikSne <commits@niksne.ru>
2026-07-25 14:57:24 +0300

    chore(noctalia-greeter): update config

--- system/programs/noctalia.nix
+++ system/programs/noctalia.nix
@@ -12,8 +12,14 @@
     enable = true;
     package = inputs.noctalia-greeter.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
-    settings.cursor.theme = let
-      inherit (config.theme) flavor accent;
-    in "catppuccin-${flavor}-${accent}-cursors";
+    settings = {
+      cursor.theme = let
+        inherit (config.theme) flavor accent;
+      in "catppuccin-${flavor}-${accent}-cursors";
+      appearance = {
+        password_style = "random";
+        hide_logo = true;
+      };
+    };
   };
 }
